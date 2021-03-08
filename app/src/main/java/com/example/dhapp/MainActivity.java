4780bd39665ed369d2ea7560e305d56a47d8911a
Marc <marcphilippschepp@gmail.com>
2021-03-08 16:32:47 +0100

    Showing ISIN now in SingleStockOverview Only Uppercase in EditText allowed

--- app/src/main/java/com/example/dhapp/MainActivity.java
+++ app/src/main/java/com/example/dhapp/MainActivity.java
@@ -4,12 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputFilter;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button confirm;
+    public EditText editText;
+    public static String StockName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +22,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
        confirm = findViewById(R.id.ButtonConfirm);
+       editText = findViewById(R.id.stockNameEditView);
        confirm.setOnClickListener(new View.OnClickListener() {
            @Override
            public void onClick(View view) {
+               StockName = editText.getText().toString();
+               System.out.println(StockName);
                changeActivity();
            }
        });
@@ -29,5 +37,7 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, SingleStockOverview.class);
         startActivity(intent);
      }
+
+
 
 }
