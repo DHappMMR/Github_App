68a4ffeec9b3eefe96170284ab254c2d54ccc2c9
Marc <marcphilippschepp@gmail.com>
2021-03-08 16:11:23 +0100

    Added working buttons

--- app/src/main/java/com/example/dhapp/MainActivity.java
+++ app/src/main/java/com/example/dhapp/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.dhapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button confirm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+       confirm = findViewById(R.id.ButtonConfirm);
+       confirm.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               changeActivity();
+           }
+       });
     }
+
+     private void changeActivity() {
+        Intent intent = new Intent(this, SingleStockOverview.class);
+        startActivity(intent);
+     }
+
 }
