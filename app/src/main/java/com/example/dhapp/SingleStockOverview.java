e7ddd0a0aab4a221611faf99740bd6be0df9b347
Marc <marcphilippschepp@gmail.com>
2021-03-09 16:29:55 +0100

    First Steps of Musterdepot

--- app/src/main/java/com/example/dhapp/SingleStockOverview.java
+++ app/src/main/java/com/example/dhapp/SingleStockOverview.java
@@ -7,6 +7,8 @@ import android.widget.TextView;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.w3c.dom.Text;
+
 public class SingleStockOverview extends AppCompatActivity {
 
     private TextView showStockName;
@@ -24,5 +26,47 @@ public class SingleStockOverview extends AppCompatActivity {
 
     public void goBack(View view) {
         finish();
+    }
+
+    public void addToDepotMethod(View view) {
+
+        TextView StockName;
+        String StringStockName;
+        StockName = findViewById(R.id.ShowClearName);
+        StringStockName = StockName.getText().toString();
+
+        TextView ISIN;
+        String StringISIN;
+        ISIN = findViewById(R.id.ShowStockName);
+        StringISIN = ISIN.getText().toString();
+
+        TextView DEPOTvalue;
+        String StringDEPOTValue;
+        DEPOTvalue = findViewById(R.id.CurrentStockValue);
+        StringDEPOTValue = DEPOTvalue.getText().toString();
+
+        TextView DEPOTmarketcap;
+        String StringDEPOTcap;
+        DEPOTmarketcap = findViewById(R.id.MarketCapStringValue);
+        StringDEPOTcap = DEPOTmarketcap.getText().toString();
+
+        TextView DEPOTvalueD;
+        String StringDEPOTvalueD;
+        DEPOTvalueD = findViewById(R.id.ValueD);
+        StringDEPOTvalueD = DEPOTvalueD.getText().toString();
+
+        TextView DEPOTvalueE;
+        String StringDEPOTvalueE;
+        DEPOTvalueE = findViewById(R.id.ValueE);
+        StringDEPOTvalueE = DEPOTvalueE.getText().toString();
+
+        TextView DEPOTvalueF;
+        String StringDEPOTvalueF;
+        DEPOTvalueF = findViewById(R.id.ValueF);
+        StringDEPOTvalueF = DEPOTvalueF.getText().toString();
+
+
+
+
     }
 }
