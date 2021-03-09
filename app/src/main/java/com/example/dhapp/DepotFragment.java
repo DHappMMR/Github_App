207cf37efce2837bd932c71e5173fc5f440c8169
Marc <marcphilippschepp@gmail.com>
2021-03-09 23:04:39 +0100

    Statisches Musterdepot erstellt

--- app/src/main/java/com/example/dhapp/DepotFragment.java
+++ app/src/main/java/com/example/dhapp/DepotFragment.java
@@ -8,6 +8,8 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class DepotFragment extends Fragment {
 
@@ -15,5 +17,23 @@ public class DepotFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_depot, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        String AktienNamen[];
+        String AktienWerte[];
+        RecyclerView recyclerView;
+
+        AktienNamen = getResources().getStringArray(R.array.AktienName);
+        AktienWerte = getResources().getStringArray(R.array.AktienWert);
+
+        recyclerView = view.findViewById(R.id.RecyclerView);
+
+        MyAdapter myAdapter = new MyAdapter(requireContext(), AktienNamen, AktienWerte);
+        recyclerView.setAdapter(myAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(requireContext()));
     }
 }
--- app/src/main/java/com/example/dhapp/MainActivity.java
+++ app/src/main/java/com/example/dhapp/MainActivity.java
@@ -8,6 +8,8 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
