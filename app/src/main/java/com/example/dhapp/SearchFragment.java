7e3015cd96945c9423968ef9905282a3f4979d51
RobinTheezy <robinkarlsruhersc@gmail.com>
2021-03-19 15:29:47 +0100

    change api request

--- app/src/main/java/com/example/dhapp/SearchFragment.java
+++ app/src/main/java/com/example/dhapp/SearchFragment.java
@@ -2,6 +2,7 @@ package com.example.dhapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.text.Editable;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -22,7 +23,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.net.URL;
-import java.nio.Buffer;
 import java.nio.charset.Charset;
 
 public class SearchFragment extends Fragment implements View.OnClickListener{
@@ -30,7 +30,7 @@ public class SearchFragment extends Fragment implements View.OnClickListener{
     private static final String accessKEY="86a7719f8f68bb10f9cbef8614745331";
 
     private static String stock = "";
-    private static String apiURL = "http://api.marketstack.com/v1/eod/?access_key=" + accessKEY + "&symbols=" + stock;
+    private static String apiURL = "http://api.marketstack.com/v1/eod/?access_key=86a7719f8f68bb10f9cbef8614745331&symbols=" + stock;
 
     private Button confirm;
     EditText stockInput;
@@ -52,44 +52,13 @@ public class SearchFragment extends Fragment implements View.OnClickListener{
 
     @Override
     public void onClick(View v){
-        final String stockInfo = stockInput.getText().toString();
         try{
-            apiURL.replace(stock, stockInfo);
-            final JSONObject searchResult = getStockInformation(apiURL);
-        }catch (IOException | JSONException e){
+            String input = stockInput.getText().toString();
+            JSONObject answer = ((MainActivity)getActivity()).getStockInformation(input);
+        }catch (Exception e){
             e.printStackTrace();
         }
 
     }
-
-    private static JSONObject getStockInformation(final String url) throws IOException, JSONException {
-
-        final InputStream inputStream = new URL(url).openStream();
-        try{
-
-            final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, Charset.forName("UTF-8")));
-            final String answerJSON = read(bufferedReader);
-
-            final JSONObject searchResult = new JSONObject(answerJSON);
-
-            return searchResult;
-        } finally {
-            inputStream.close();
-        }
-    }
-
-    private static String read(final Reader reader)throws IOException, JSONException{
-
-        final StringBuilder stringBuilder = new StringBuilder();
-
-        int count;
-
-        while((count = reader.read())!= -1){
-            stringBuilder.append((char)count);
-        }
-
-        return stringBuilder.toString();
-    }
-
 }
 
