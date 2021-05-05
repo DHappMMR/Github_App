bb76132f8c82f38ecfa8c7c9bb42a5ce6e991ab0
Marc Brodam <marc.brodam@gmx.de>
2021-05-05 18:39:23 +0200

    Added Basic Database

--- app/src/main/java/com/example/dhapp/MainActivity.java
+++ app/src/main/java/com/example/dhapp/MainActivity.java
@@ -34,6 +34,8 @@ public class MainActivity extends AppCompatActivity  {
     private static String url="http://api.marketstack.com/v1/eod?access_key=86a7719f8f68bb10f9cbef8614745331&symbols=";
     private static String apiURLName;
 
+    private DbManager _datenbankManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -42,6 +44,9 @@ public class MainActivity extends AppCompatActivity  {
 
         BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
         bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        //Datenbank
+        _datenbankManager = new DbManager(this);
 
         showStockName = findViewById(R.id.ShowStockName);
 
