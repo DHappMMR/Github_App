3c5b026af5b0f9607645afc38e5a1814754d31b9
Marc432341 <marc.brodam@gmx.de>
2021-05-05 18:11:12 +0200

    Datenbank

--- app/src/main/java/com/example/dhapp/DbManager.java
+++ app/src/main/java/com/example/dhapp/DbManager.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 import javax.xml.namespace.QName;
 
@@ -15,31 +16,40 @@ public class DbManager extends SQLiteOpenHelper {
                 null,
                 1);
     }
+
     @Override
     public void onCreate(SQLiteDatabase db) {
         try {
+
+
             db.execSQL("CREATE TABLE name (" +
                     "symbole TEXT PRIMARY KEY NOT NULL, " +
                     "stockname TEXT)"
             );
 
             db.execSQL("CREATE TABLE value ( " +
-                    "symb INTEGER PRIMARY KEY," +
+                    "symb TEXT PRIMARY KEY NOT NULL," +
                     "value INTEGER," +
                     "marketCap INTEGER," +
                     "volume INTEGER," +
                     "FOREIGN KEY (symb) REFERENCES name(symbole) )"
                     );
 
+            db.execSQL("INSERT INTO value (symb, value, marketCap, volume) VALUES (APPL, 108, 1850000000, 6000)");
 
+            db.execSQL("INSERT INTO name (symbole, stockname) VALUES (APPL, Apple_Inc)");
+
+            System.out.println("Correct");
+            Log.e("dbCorrect", "Erfolgreich");
 
         } catch (SQLException e) {
-            e.printStackTrace();
+            Log.d("dbFail", "Exception bei Create Methode" + e);
+            System.out.println("Fail");
         }
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        //leer
     }
 }
--- app/src/main/java/com/example/dhapp/MainActivity.java
+++ app/src/main/java/com/example/dhapp/MainActivity.java
@@ -1,13 +1,38 @@
 package com.example.dhapp;
 
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.IntentSender;
+import android.content.ServiceConnection;
+import android.content.SharedPreferences;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.content.res.AssetManager;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.os.StrictMode;
+import android.os.UserHandle;
+import android.util.Log;
+import android.view.Display;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -19,6 +44,10 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -30,16 +59,20 @@ public class MainActivity extends AppCompatActivity  {
 
 
     private Button confirm;
-    public TextView showStockName;
+     public TextView showStockName;
     private static String stock="";
 
     private static String url="http://api.marketstack.com/v1/eod/latest?access_key=86a7719f8f68bb10f9cbef8614745331&symbols=";
+    private DbManager _datenbankManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Datenbank
+        _datenbankManager = new DbManager(this);
 
         BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
         bottomNav.setOnNavigationItemSelectedListener(navListener);
@@ -51,6 +84,7 @@ public class MainActivity extends AppCompatActivity  {
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new SearchFragment()).commit();
         }
+
 
     }
 
@@ -85,6 +119,9 @@ public class MainActivity extends AppCompatActivity  {
 
 
     protected JSONObject getStockInformation(String ISIN) throws Exception {
+        Log.e("test12344321", "test1234");
+        System.out.println("test1234");
+
         URL link = null;
         HttpURLConnection conn = null;
         String object="";
@@ -117,4 +154,5 @@ public class MainActivity extends AppCompatActivity  {
         JSONObject jsonObject = new JSONObject(json);
         return jsonObject;
     }
+
 }
